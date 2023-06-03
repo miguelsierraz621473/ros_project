b059edf825066e63e32f7e5bc9ce591df6020b04
miguelsierraz621473 <110655618+miguelsierraz621473@users.noreply.github.com>
2023-06-03 11:36:04 +0900

    Level Up [MUST IMPROVE]

--- src/controller_py/src/wander_controller.py
+++ src/controller_py/src/wander_controller.py
@@ -4,6 +4,12 @@ import rospy
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 import threading
+import enum
+
+class RotationDirection(enum.Enum):
+    NOT_DEFINED = 0
+    CLOCKWISE = 1
+    COUNTERCLOCKWISE = 2
 
 class WanderController(threading.Thread):
 
@@ -15,37 +21,71 @@ class WanderController(threading.Thread):
         self.twist_publisher = rospy.Publisher("/cmd_vel", Twist, queue_size=1)
         self.obstacle_ahead = False
         self.minimum_acceptable_range = minimum_acceptable_range
-        self.minimum_range_ahead = None
-        self.state_change_time = rospy.Time.now()
+        self.angle_delta = 15
+        self.current_ranges = tuple()
+        self.minimum_distance = None
+        self.minimum_distance_angle = None
+        self.rotation = RotationDirection.NOT_DEFINED
         self.rate = rospy.Rate(10)
         rospy.loginfo("Python Controller has been initialized")
         
-    def scanCallback(self, msg : LaserScan):
+    def scanCallback(self, msg : LaserScan) -> None:
 
-        self.minimum_range_ahead = min(msg.ranges)
+        self.current_ranges = msg.ranges
+        self.minimum_distance = min(self.current_ranges)
+        self.minimum_distance_angle = self.current_ranges.index(self.minimum_distance)
+        rospy.loginfo("Values Minimum Range: %f Angle: %f Type: %s", self.minimum_distance, self.minimum_distance_angle, str(type(self.current_ranges)))
 
-    def wanderFunction(self):
+    def define_rotation_direction(self, minimum_distance: float, minimum_range_angle: float, whole_ranges: tuple, angle_delta: int) -> RotationDirection:
+
+        if minimum_distance is None or minimum_range_angle is None or len(whole_ranges) == 0:
+            return RotationDirection.NOT_DEFINED
+        
+        angle_right = minimum_range_angle + angle_delta
+        angle_left = minimum_range_angle - angle_delta
+
+        distance_right = whole_ranges[angle_right]
+        distance_left = whole_ranges[angle_left]
+
+        if distance_left < minimum_distance < distance_right:
+            return RotationDirection.CLOCKWISE
+        elif distance_left > minimum_distance > distance_right:
+            return RotationDirection.COUNTERCLOCKWISE
+        else:
+            return RotationDirection.NOT_DEFINED
+
+    def wanderFunction(self) -> None:
 
         while not rospy.is_shutdown():
 
             try:
+
+                if self.minimum_distance is None or self.minimum_distance_angle is None:
+                    continue
             
                 if not self.obstacle_ahead:
-                    if (self.minimum_acceptable_range < self.minimum_acceptable_range or rospy.Time.now() > self.state_change_time):
+                    if (self.minimum_distance < self.minimum_acceptable_range):
                         self.obstacle_ahead = True
-                        self.state_change_time = rospy.Time.now() + rospy.Duration(5)
 
                 else:
-                    if (rospy.Time.now() > self.state_change_time):
+                    if (self.minimum_distance > self.minimum_acceptable_range):
                         self.obstacle_ahead = False
-                        self.state_change_time = rospy.Time.now() + rospy.Duration(30)
+
 
                 twist_to_publish = Twist()
 
                 if not self.obstacle_ahead:
-                    twist_to_publish.linear.x = 1
+                    twist_to_publish.linear.x = 0.5
+                    twist_to_publish.angular.z = 0.0
                 else:
-                    twist_to_publish.angular.z = 1
+                    rotation_direction = self.define_rotation_direction(self.minimum_distance, self.minimum_distance_angle, self.current_ranges, self.angle_delta)
+                    if rotation_direction == RotationDirection.CLOCKWISE or rotation_direction == RotationDirection.NOT_DEFINED:
+                        sign = 1
+                    elif rotation_direction == RotationDirection.COUNTERCLOCKWISE:
+                        sign = -1
+
+                    twist_to_publish.linear.x = 0.0
+                    twist_to_publish.angular.z = sign*0.5
 
                 self.twist_publisher.publish(twist_to_publish)
 
