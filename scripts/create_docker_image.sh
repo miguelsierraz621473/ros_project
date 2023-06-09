df26913e45e150252bdeb084a634c582338e295f
miguelsierraz621473 <110655618+miguelsierraz621473@users.noreply.github.com>
2023-06-09 09:17:05 +0900

    Solved Docker image creation

--- scripts/create_docker_image.sh
+++ scripts/create_docker_image.sh
@@ -5,4 +5,4 @@ script_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd $script_path
 cd ..
 cd docker
-docker build -t $DOCKER_IMAGE_NAME .
+docker build --progress plain -t $DOCKER_IMAGE_NAME . 
